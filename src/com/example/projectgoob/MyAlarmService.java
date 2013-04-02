ee213c1e58de215dad68905116626b998165021b
Harry Rybacki <hrybacki@gmail.com>
2013-04-01 21:27:43 -0300

    Was missing documentation. Comments didn't follow standards.

--- src/com/example/projectgoob/MyAlarmService.java
+++ src/com/example/projectgoob/MyAlarmService.java
@@ -1,5 +1,6 @@
 package com.example.projectgoob;
 
+// android imports
 import android.app.Notification;
 import android.app.Service;
 import android.content.Intent;
@@ -10,11 +11,12 @@ import android.media.audiofx.BassBoost.Settings;
 import android.net.Uri;
 import android.os.IBinder;
 
+// responsible for starting and stopping alarms
 public class MyAlarmService extends Service {
 	
 	public void onCreate()
 	{
-		
+		// pass
 	}
 
 	@Override
@@ -24,19 +26,16 @@ public class MyAlarmService extends Service {
 	}
 	public void onDestroy(){
 		super.onDestroy();
-	}
+	}	
 	
-	
+	// when service is called calls its respective intent
 	@SuppressWarnings("deprecation")
 	public void onStart(Intent intent, int startId){
 		super.onStart(intent, startId);
 		Intent alertIntent = new Intent();
 		alertIntent.setClass(this, MathActivity.class);
 		alertIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK); 
+		// launch desired intent
 		startActivity(alertIntent);
-		
-		
-		
 	}
-
 }
