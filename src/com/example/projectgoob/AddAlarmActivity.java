6bfd4aec847c12aeab98ebda02491a00fc659e33
Harry Rybacki <hrybacki@gmail.com>
2013-04-01 21:20:02 -0300

    Was missing documentation. Comments didn't follow standards.

--- src/com/example/projectgoob/AddAlarmActivity.java
+++ src/com/example/projectgoob/AddAlarmActivity.java
@@ -1,7 +1,10 @@
+// package
 package com.example.projectgoob;
 
+// java imports
 import java.util.Calendar;
 
+// android imports
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlarmManager;
@@ -13,54 +16,54 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.TimePicker;
 
+// activity for setting, adding, and implementing a new alarm
 public class AddAlarmActivity extends Activity {
 	
 	TimePicker tp;
 
+        // set layout for the activity
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_alarm);
 	}
 
+        // set menu
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
+		// inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.add_alarm, menu);
 		return true;
 	}
 	
+	// complete the actvity
 	public void finish(View v)
 	{
 		finish();
 	}
 	
+	// add the alarm to the OS's pending intents
 	public void saveAlarm(View v)
 	{
 		tp = (TimePicker) findViewById(R.id.timepicker);
-		//AlertDialog alertDialog = new AlertDialog.Builder(AddAlarmActivity.this).create();
-		//alertDialog.setMessage(tp.getCurrentHour().toString());
-		//alertDialog.show();
-		//create alarm intent 
+		// create alarm intent 
 		Intent alarmIntent = new Intent(AddAlarmActivity.this, MyAlarmService.class);
-		//create pending alarm intent to link to service (what does the waiting)
+		// create pending alarm intent to link to service (what does the waiting)
 		PendingIntent pendingAlarmIntent = PendingIntent.getService(AddAlarmActivity.this, 
 				0, alarmIntent, 0);
-
-		//create Alarm manager to let android system know about the alarm
+		// create Alarm manager to let android system know about the alarm
 		AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
-
-		//create calendar using android system to set user's alarm
+		// create calendar using android system to set user's alarm
 		Calendar AlarmCal = Calendar.getInstance();
-		//set milliseconds to current milliseconds
+		// set milliseconds to current milliseconds
 		AlarmCal.setTimeInMillis(System.currentTimeMillis());
-		//set alarmCal's hour and minute to user selections
+		// set alarmCal's hour and minute to user selections
 		AlarmCal.set(Calendar.HOUR_OF_DAY, tp.getCurrentHour());
 		AlarmCal.set(Calendar.MINUTE, tp.getCurrentMinute());
 		AlarmCal.set(Calendar.SECOND, 0);
 
-		//Tell the alarm manager when to run the service
-		//RTC_WAKEUP wakes up device if it is asleep when alarm is triggered
+		// tell the alarm manager when to run the service
+		// RTC_WAKEUP wakes up device if it is asleep when alarm is triggered
 		alarmManager.set(AlarmManager.RTC_WAKEUP,
 		AlarmCal.getTimeInMillis(), 
 		pendingAlarmIntent);
@@ -69,14 +72,11 @@ public class AddAlarmActivity extends Activity {
 
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
-				// TODO Auto-generated method stub
+				// @TODO Auto-generated method stub
 
 			} 
-
+        // load the time picker onto the sreen
 	}).create().show();
-		
-		
 		finish();
 	}
-
 }
